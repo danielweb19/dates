5d1bf419e994809bcb2e80797e15a8730ff32a78
Daniel <daniel.web19@outlook.com>
2020-08-01 14:02:26 -0500

    primer respositorio

---
+++ src/index.js
@@ -0,0 +1,49 @@
+import './style.css';
+//import view from './x.html';
+import moment from 'moment';
+
+
+//moment.locale('es');
+//document.querySelector('h1').innerText = `hoy es ${ndia} ${dia} de ${mes} del ${año}`;
+
+/** Initial title */
+let nextYear =  Number(moment().add(1,'year').format('YYYY'));
+const difi = document.querySelector('.falta');
+difi.innerText = `there are ${calcDiff('days',nextYear)} days until ${nextYear}`;
+
+
+/* Progress bar */
+export let porcentaje = 100 - Math.round(calcDiff('days',nextYear)/3.65);
+const barra = document.getElementById('interporcent');
+barra.style.width = `${porcentaje}%`;
+barra.innerText = `${porcentaje}%`;
+
+
+
+/** Functions */
+
+function calcDiff(unity,target){
+    let diference = moment().diff(target+'-01-01',unity);
+    return diference *=-1;
+}
+
+/** listen options */
+
+const options = document.querySelector('.op');
+
+options.addEventListener('click',(e)=>{
+    
+    const unity = e.target.innerText;
+    if(unity.length <15){ 
+        difi.innerText = `There are ${new Intl.NumberFormat().format(calcDiff(unity.toLowerCase(),nextYear))} ${unity} until ${nextYear}`;
+    }
+});
+
+/** p5 */
+
+const x = document.querySelector('.x');
+
+
+
+
+
---
+++ src/sketch.js
@@ -0,0 +1,24 @@
+let box = document.querySelector('body');
+let w = box.clientWidth,
+    h = box.clientHeight;
+//import {porcentaje} from './index';
+
+box.addEvent(window, "resize", function(e) {
+    w = box.clientWidth;
+    h = box.clientHeight;
+});
+function setup(){
+    createCanvas(w,h);
+}
+
+function draw(){
+    //background(0);
+    translate(w/2,h/2);
+    stroke(50,255,255);
+    rotate (frameCount*0.01);
+    line(0,0,w/-3.5,0);
+}
+
+function windowResized() {
+    resizeCanvas(w,h);
+}
